6f212f805f1c0d71fe274d76718be0eb32291ba0
Vndrvs <99025350+Vndrvs@users.noreply.github.com>
2024-11-09 10:34:54 +0100

    updated 5th to adequate form

--- valtozokOt.py
+++ valtozokOt.py
@@ -1,3 +1,24 @@
+# 5. feladat Kérjünk be két számot úgy, hogy később matematikai műveleteket tudjunk
+# rá alkalmazni. Ezek lesznek egy téglalap oldalai. Írassuk ki a területét, kerületét a kiírás
+# mellé és alá
+
+a = int(input("Kérem az a oldal hosszát! "))
+b = int(input("Kérem a b oldal hosszát! "))
+
+k = 2*(a+b)
+t = (a*b)
+
+print("A téglalap kerülete: ", str(k))
+print("A téglalap kerülete: ", k)
+print("A téglalap kerülete: ", end='')
+print(k)
+
+print("A téglalap területe: ", t)
+print("A téglalap területe: ", t, sep='\n')
+print("A téglalap területe: ", t,t,t, sep='\n')
+     
+      
+""""
 a = int(input("Please input edge 1 (cm): "))
 b = int(input("Please input edge 2 (cm): "))
 
@@ -7,6 +28,5 @@ perimeter = 2 * (a + b)
 
 print(2* (f"The perimeter of the rectangle is: " + str(perimeter) + "cm\n"))
 print("The perimeter of the rectangle is: ", str(perimeter), sep='\n')
-print(f"The perimeter of the rectangle is: ", 3*(perimeter + '\n'), sep='\n')
-      
+"""
 
